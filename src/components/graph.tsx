d276200c73eda4bba3ade6f84f8d9ae0f34aee59
Adam Berri <adamberri@adams-mac-mini.lan>
2021-11-27 07:27:01 -0500

    added nav bars with 2x2 grid

--- src/components/graph.tsx
+++ src/components/graph.tsx
@@ -5,20 +5,7 @@ import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import { purple } from '@mui/material/colors';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      // Purple and green play nicely together.
-      main: purple[500],
-    },
-    secondary: {
-      // This is green.A700 as hex.
-      main: '#11cb5f',
-    },
-  },
-});
-
-
+const theme = createTheme({ palette: { mode: 'dark' } });
 
 export default function graph() {
   return (
--- src/react.tsx
+++ src/react.tsx
@@ -3,19 +3,22 @@ import ReactDOM from 'react-dom';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
 import Graph from './components/graph.tsx';
+import './scss/styles.scss';
+import ResponsiveAppBar from './components/navbar.tsx';
 
 export default function App() {
   return (
     <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+  <ResponsiveAppBar></ResponsiveAppBar>
   <Grid item xs={6} md={6}>
-  <Paper variant="outlined" square />
+  <Graph></Graph>
   </Grid>
   <Grid item xs={6} md={6}>
-  <Paper variant="outlined" square />
+  <Graph></Graph>
 
   </Grid>
   <Grid item xs={6} md={6}>
-  <Paper variant="outlined" square />
+  <Graph></Graph>
 
   </Grid>
   <Grid item xs={6} md={6}>
