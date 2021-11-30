0f5b73148be505d85e6a89528c7e3450dfe531da
Adam Berri <adamberri@adams-mac-mini.lan>
2021-11-30 07:51:54 -0500

    collapsable trees with object, route for SQL needs to be written, more styling needed

--- src/components/Tree.tsx
+++ src/components/Tree.tsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect } from "react";
-import { select, hierarchy, tree, linkHorizontal } from "d3";
-import useResizeObserver from "./useResizeObserver.jsx";
+import Tree from 'react-d3-tree';
 
 function usePrevious(value) {
   const ref = useRef();
@@ -11,124 +10,47 @@ function usePrevious(value) {
 }
 
 function TreeChart() {
-  var data = 
-    {
-      name: "Top Level",
-      parent: "null",
-      children: [
-        {
-          name: "Level 2: A",
-          parent: "Top Level",
-          children: [
-            {
-              name: "Son of A",
-              parent: "Level 2: A"
-            },
-            {
-              name: "Daughter of A",
-              parent: "Level 2: A"
-            }
-          ]
+  const data = {
+    name: 'CEO',
+    children: [
+      {
+        name: 'Manager',
+        attributes: {
+          department: 'Production',
         },
-        {
-          name: "Level 2: B",
-          parent: "Top Level"
-        }
-      ]
-    }
-  ;
-  console.log('reaching treechart')
-  const svgRef = useRef();
-  const wrapperRef = useRef();
-  const dimensions = useResizeObserver(wrapperRef);
+        children: [
+          {
+            name: 'Foreman',
+            attributes: {
+              department: 'Fabrication',
+            },
+            children: [
+              {
+                name: 'Worker',
+              },
+            ],
+          },
+          {
+            name: 'Foreman',
+            attributes: {
+              department: 'Assembly',
+            },
+            children: [
+              {
+                name: 'Worker',
+              },
+            ],
+          },
+        ],
+      },
+    ],
+  };
 
-  // we save data to see if it changed
-  const previouslyRenderedData = usePrevious(data);
-
-  // will be called initially and on every data change
-  useEffect(() => {
-    const svg = select(svgRef.current);
-
-    // use dimensions from useResizeObserver,
-    // but use getBoundingClientRect on initial render
-    // (dimensions are null for the first render)
-    const { width, height } =
-      dimensions || wrapperRef.current.getBoundingClientRect();
-
-    // transform hierarchical data
-    const root = hierarchy(data);
-    const treeLayout = tree().size([height, width]);
-
-    const linkGenerator = linkHorizontal()
-      .x(link => link.y)
-      .y(link => link.x);
-
-    // enrich hierarchical data with coordinates
-    treeLayout(root);
-
-    console.warn("descendants", root.descendants());
-    console.warn("links", root.links());
-
-    // nodes
-    svg
-      .selectAll(".node")
-      .data(root.descendants())
-      .join(enter => enter.append("circle").attr("opacity", 0))
-      .attr("class", "node")
-      .attr("cx", node => node.y)
-      .attr("cy", node => node.x)
-      .attr("r", 4)
-      .transition()
-      .duration(500)
-      .delay(node => node.depth * 300)
-      .attr("opacity", 1);
-
-    // links
-    const enteringAndUpdatingLinks = svg
-      .selectAll(".link")
-      .data(root.links())
-      .join("path")
-      .attr("class", "link")
-      .attr("d", linkGenerator)
-      .attr("stroke-dasharray", function() {
-        const length = this.getTotalLength();
-        return `${length} ${length}`;
-      })
-      .attr("stroke", "black")
-      .attr("fill", "none")
-      .attr("opacity", 1);
-
-    if (data !== previouslyRenderedData) {
-      enteringAndUpdatingLinks
-        .attr("stroke-dashoffset", function() {
-          return this.getTotalLength();
-        })
-        .transition()
-        .duration(500)
-        .delay(link => link.source.depth * 500)
-        .attr("stroke-dashoffset", 0);
-    }
-
-    // labels
-    svg
-      .selectAll(".label")
-      .data(root.descendants())
-      .join(enter => enter.append("text").attr("opacity", 0))
-      .attr("class", "label")
-      .attr("x", node => node.y)
-      .attr("y", node => node.x - 12)
-      .attr("text-anchor", "middle")
-      .attr("font-size", 24)
-      .text(node => node.data.name)
-      .transition()
-      .duration(500)
-      .delay(node => node.depth * 300)
-      .attr("opacity", 1);
-  }, [data, dimensions, previouslyRenderedData]);
 
   return (
-    <div ref={wrapperRef} style={{ marginBottom: "2rem", paddingLeft: "20px" }}>
-      <svg ref={svgRef}></svg>
+    // `<Tree />` will fill width/height of its container; in this case `#treeWrapper`.
+    <div id="treeWrapper" style={{ width: '50em', height: '20em' }}>
+      <Tree data={data} />
     </div>
   );
 }
