cf44460cd8f1c1b04df0c5163284c59dadcbeda7
Alex Barbazan <alexbarbazan@Alexs-MacBook-Pro.local>
2021-12-05 15:07:02 -0500

    local changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Provider } from 'react-redux';
-import App from './react';
-import store from './store';
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { Provider } from 'react-redux';
+// import App from './react';
+// import store from './store';
 
 
-render(
-  // wrap the App in the Provider Component and pass in the store
-  <Provider store = {store} > <App/> </Provider>,
-  document.getElementById('root')
-);
+// render(
+//   // wrap the App in the Provider Component and pass in the store
+//   <Provider store = {store} > <App/> </Provider>,
+//   document.getElementById('root')
+// );
