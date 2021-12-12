b77ab30c8ba9731af85b1f70f6d4eac0e65d2be8
Adam Berri <78669179+AdamBerri@users.noreply.github.com>
2021-12-12 05:34:40 -0500

    Revert "Alex dec11 branch"

--- server/models/projectDB.ts
+++
@@ -1,8 +0,0 @@
-import { PoolWrapper } from '../types/PoolWrapper';
-
-// To be updated by state
-const PG_URI: string = `postgres://vozivmzl:6vudzc_5vjGcmGFKOClHXhcJfXLW-QXB@fanny.db.elephantsql.com/vozivmzl`;
-
-const db = new PoolWrapper(PG_URI);
-
-export default db;
--- src/services/auth-header.js
+++
@@ -1,8 +0,0 @@
-/*
-Checks Local Storage for user item. If there is a logged in user with accessToken (JWT), 
-return HTTP Authorization header. Otherwise, return an empty object.
-*/
-export function authHeader() {
-  const user = JSON.parse(localStorage.getItem('user'));
-  return user && user.accessToken ? { 'x-access-token': user.accessToken } : {};
-}
