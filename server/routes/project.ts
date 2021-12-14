ce8f1b49431aa62b11bb96f8e419266bccbfff2f
jwmaguire15 <jmaguire655@gmail.com>
2021-12-14 17:14:51 -0500

    Query strings (#29)
    * james query string stuff
    
    * resolver string and typeDefs working
    
    * clean up
    
    * more cleanup
    
    * resolver string just a string now
    
    * conflicts

--- server/routes/project.ts
+++ server/routes/project.ts
@@ -2,20 +2,9 @@ import express, { Request, Response, NextFunction } from 'express';
 import { projectDBController } from '../controllers/projectDBController';
 import { rowsToD3, rowsToTable } from '../SQLConversion/SQLSchemaHelpers';
 const projectRouter = express.Router();
-<<<<<<< HEAD
 const { updateDatabase, getAllTables, buildTypeDefs, buildResolvers, buildSchema } = projectDBController;
 
 projectRouter.post('/tables', updateDatabase, getAllTables, buildTypeDefs, buildResolvers, buildSchema, (req: Request, res: Response, next: NextFunction) => {
-=======
-const { updateDatabase, getAllTables, createObjectTypes, createQueryTypes, createMutationsTypes, buildResolvers, buildSchema } = projectDBController;
-
-//returns all tables and relevant SQL schema from user's provided db 
-// projectRouter.get('/tables', getAllTables, createObjectTypes, createQueryTypes, createMutationsTypes, buildResolvers, (req: Request, res: Response, next: NextFunction) => {
-//   return res.json(rowsToTable(res.locals.userDbResponse));
-// });
-
-projectRouter.post('/tables', updateDatabase, getAllTables, createObjectTypes, createQueryTypes, createMutationsTypes, buildResolvers, buildSchema, (req: Request, res: Response, next: NextFunction) => {
->>>>>>> dev
   return res.status(201).json({
     typeDefs: res.locals.typeDefs,
     resolverString: res.locals.resolverString,
@@ -26,10 +15,6 @@ projectRouter.get('/tables', getAllTables, (req: Request, res: Response, next: N
   return res.status(200).json(rowsToTable(res.locals.userDbResponse));
 });
 
-<<<<<<< HEAD
-=======
-
->>>>>>> dev
 //returns all tables and relevant SQL schema from user's provided db in desired D3 form 
 projectRouter.post('/D3tables', updateDatabase, getAllTables, (req: Request, res: Response, next: NextFunction) => {
   return res.status(201).json(rowsToD3(res.locals.userDbResponse));
