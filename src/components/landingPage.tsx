7f7682dc4458c6a591034e4168d824e7b58955ce
jwmaguire15 <jmaguire655@gmail.com>
2021-12-15 22:48:05 -0500

    James tree (#38)
    * james tree edits
    
    * james pretty print edits
    
    * tree stuff

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -13,26 +13,26 @@ import Sandbox from './sandbox';
 
 //add the conditional rendering right under the Grid
 
-export default function LandingPage(props:any) {
-  
+export default function LandingPage(props: any) {
+
   return (
 
-  <>
-  <ResponsiveAppBar style={{minWidth: "100%"}} setUriBoolean={props.setUriBoolean}></ResponsiveAppBar>
-  {(!props.graphiql) ?   
-  <Grid container rowSpacing={1.5} columnSpacing={{ xs: 1, sm: 2, md: 1.5}} style={{
-  minWidth: "100%",
-  height: "92vh",
-  padding: '.8% 0% 0% .0%'
-}}>
-  <Grid item xs={6} md={9} >
-  <Graph uriString={props.uriString}></Graph>
-  </Grid>
-  <Grid item xs={6} md={3} >
-  <Resolvers uriString={props.uriString}/>
-  </Grid>
-</Grid> :
-  <div></div>}
-  </>
- )
+    <>
+      <ResponsiveAppBar style={{ minWidth: "100%" }} setUriBoolean={props.setUriBoolean}></ResponsiveAppBar>
+      {(!props.graphiql) ?
+        <Grid container={true} rowSpacing={1.5} columnSpacing={{ xs: 1, sm: 2, md: 1.5 }} style={{
+          minWidth: "100%",
+          height: "92vh",
+          padding: '.8% 0% 0% .0%'
+        }}>
+          <Grid item xs={6} md={7} >
+            <Graph uriString={props.uriString}></Graph>
+          </Grid>
+          <Grid item xs={6} md={5} >
+            <Resolvers uriString={props.uriString} />
+          </Grid>
+        </Grid> :
+        <div></div>}
+    </>
+  )
 }
