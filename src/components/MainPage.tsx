29d79cf5a4a440d5063c4ba98c24c0c568daeb3d
Alex Barbazan <alexbarbazan@Alexs-MacBook-Pro.local>
2021-12-16 02:32:29 -0500

    moving along

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -14,13 +14,29 @@ import Sandbox from './sandbox';
 //add the conditional rendering right under the Grid
 
 export default function LandingPage(props: any) {
-
+  const[uriBtnClose, setHandleUriBtnClose] = useState(false);
   return (
+ 
+      
    <div className="mainDisplayContainer">
-      <ResponsiveAppBar setUriBoolean={props.setUriBoolean}></ResponsiveAppBar>
+     
+      <ResponsiveAppBar uriBtnClose = {uriBtnClose} setUriBoolean={props.setUriBoolean}></ResponsiveAppBar>
+      {!uriBtnClose && <div className="uri-box">
+        <form>
+          <div className="enterUri">
+      <h2 >Enter URI</h2>       
+      <button onClick={()=>setHandleUriBtnClose(true)}><span>Exit</span></button>
+      </div>
+             <div className="form-group">
+              <label htmlFor="uri">Enter URI</label>
+              <input type="text" className='form-control'/>
+            </div>
+        </form>
+     </div>
+      }
       {!props.graphiql && (
         <div className="mainDisplayWrapper">
-          <Graph  uriString={props.uriString} />            
+          <Graph uriString={props.uriString} />            
           <Resolvers uriString={props.uriString} />
         </div>)
       }
