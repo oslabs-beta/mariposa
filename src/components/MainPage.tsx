b5eb6b4118e35a9d91119e708f3641530b6e67dc
jwmaguire15 <jmaguire655@gmail.com>
2021-12-16 15:08:14 -0500

    deleted a lot of stuff

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -6,14 +6,6 @@ import Resolvers from './Resolvers';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 
-// style={{
-//   border: "solid",
-//   minWidth: "100%",
-//   height: "100vh",
-// }}
-
-//add the conditional rendering right under the Grid
-
 export default function LandingPage(props: any) {
   const [uriBtnClose, setHandleUriBtnClose] = useState(false);
   const [uriString, setUriString] = useState('');
@@ -54,13 +46,13 @@ export default function LandingPage(props: any) {
       })
   }
 
-  const handleClick = (uriString: string, e:any) => {
-      e.preventDefault()
-      setHandleUriBtnClose(true); //closes uri box
-      setUriString(''); //cleans uri bar
-      obtainResolvers(uriString);
-      obtainTreeData(uriString);
-      console.log(text, schema, treeData)
+  const handleClick = (uriString: string, e: any) => {
+    e.preventDefault()
+    setHandleUriBtnClose(true); //closes uri box
+    setUriString(''); //cleans uri bar
+    obtainResolvers(uriString);
+    obtainTreeData(uriString);
+    console.log(text, schema, treeData)
   }
 
   console.log('latest string', uriString)
@@ -70,42 +62,28 @@ export default function LandingPage(props: any) {
 
       <ResponsiveAppBar uriBtnClose={uriBtnClose} setHandleUriBtnClose={setHandleUriBtnClose} setUriBoolean={props.setUriBoolean}></ResponsiveAppBar>
       {!uriBtnClose && <div className="uri-box">
-        {/* <form onSubmit={(e) => handleClick(uriString, e)}>
-
-          <div className="enterUri">
-            <h2 >Enter URI</h2>
-            <button onClick={() => setHandleUriBtnClose(true)}><span>Close</span></button>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="uri">URI:</label>
-            <input type="text" className='form-control' onChange={(e) => setUriString(e.target.value)} />
-          </div>
-          <button type="submit" id="submitBtn">Submit</button>
-
-        </form> */}
-        <Button style={{width: "20%", marginLeft: "80%"}} onClick={() => setHandleUriBtnClose(true)}><span>Close</span></Button>
-        <TextField label={'URI Input....'} id="URI" onChange={(e) => setUriString(e.target.value)}/>
-        <div className='uriButtons'> 
-          <Button style={{width: "33%"}} onClick={(e) => handleClick(uriString, e)}>Submit</Button>
-          <Button style={{width: "33%"}} onClick={(e) => handleClick('', e)}>Sample Data</Button>
+        <Button style={{ width: "20%", marginLeft: "80%" }} onClick={() => setHandleUriBtnClose(true)}><span>Close</span></Button>
+        <TextField label={'URI Input....'} id="URI" onChange={(e) => setUriString(e.target.value)} />
+        <div className='uriButtons'>
+          <Button style={{ width: "33%" }} onClick={(e) => handleClick(uriString, e)}>Submit</Button>
+          <Button style={{ width: "33%" }} onClick={(e) => handleClick('', e)}>Sample Data</Button>
         </div>
       </div>
       }
-        <Grid container={true} rowSpacing={1.5} columnSpacing={{ xs: 1, sm: 2, md: 1.5 }} style={{
-          minWidth: "100%",
-          height: "92vh",
-          padding: '.8% 0% 0% .0%'
-        }}>
-          <Grid item xs={6} md={7} >
-            <Graph treeData={treeData} />
-          </Grid>
-          <Grid item xs={6} md={5} >
-            <Resolvers text={text} schema={schema} />
-          </Grid>
+      <Grid container={true} rowSpacing={1.5} columnSpacing={{ xs: 1, sm: 2, md: 1.5 }} style={{
+        minWidth: "100%",
+        height: "92vh",
+        padding: '.8% 0% 0% .0%'
+      }}>
+        <Grid item xs={6} md={7} >
+          <Graph treeData={treeData} />
         </Grid>
-    
-      
+        <Grid item xs={6} md={5} >
+          <Resolvers text={text} schema={schema} />
+        </Grid>
+      </Grid>
+
+
     </div>
   )
 }
--- src/components/Resolvers.tsx
+++ src/components/Resolvers.tsx
@@ -6,58 +6,53 @@ import ButtonGroup from '@mui/material/ButtonGroup';
 import Box from '@mui/material/Box';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
 const theme = createTheme({ palette: { mode: 'light' } });
-
 import Highlight from 'react-highlight';
 
-// import '../../node_modules/highlight.js/styles/github.css'
-
-
-
-export default function Resolvers(props:any) {
+export default function Resolvers(props: any) {
 
   //state for the user defaulted to resolvers
   const [resolver, setResolver] = useState(false);
   const [text, setText] = useState('');
   const [schema, setSchema] = useState('');
-  
+
   return (
 
-      <ThemeProvider theme={theme} >
-        <Paper elevation={2} color='primary' style={{
-          border: "gray",
-          height: "100%",
-          // paddingLeft: "5%",
-          maxHeight: '97vh',
-          backgroundColor: "#ffedff",
-          opacity: ".99",
-        }}>
-          <div className='resolvers' >
-            <Box
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                '& > *': {
-                  m: 1,
-                },
-              }}
-            >
-              <ButtonGroup className="buttonGroup" variant="outlined" aria-label="outlined button group">
-                <Button onClick={() => setResolver(false)}>TypeDefs</Button>
-                <Button onClick={() => setResolver(true)}>Resolvers</Button>
-                <Button onClick={() => { navigator.clipboard.writeText((resolver) ? props.schema : props.text) }}><ContentPasteIcon /></Button>
-              </ButtonGroup>
-            </Box>
+    <ThemeProvider theme={theme} >
+      <Paper elevation={2} color='primary' style={{
+        border: "gray",
+        height: "100%",
+        // paddingLeft: "5%",
+        maxHeight: '97vh',
+        backgroundColor: "#ffedff",
+        opacity: ".99",
+      }}>
+        <div className='resolvers' >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              '& > *': {
+                m: 1,
+              },
+            }}
+          >
+            <ButtonGroup className="buttonGroup" variant="outlined" aria-label="outlined button group">
+              <Button onClick={() => setResolver(false)}>TypeDefs</Button>
+              <Button onClick={() => setResolver(true)}>Resolvers</Button>
+              <Button onClick={() => { navigator.clipboard.writeText((resolver) ? props.schema : props.text) }}><ContentPasteIcon /></Button>
+            </ButtonGroup>
+          </Box>
 
-            <Box style={{ backgroundColor: 'transparent', margin: '0% 0% 0% 0%', width: "100%", minHeight: '93%', maxHeight: '93%', overflowY: "scroll", padding: "0px 0px 0px 0px" }}>
-              
-                <Highlight  className="javascript">
-                  {resolver ? props.schema : props.text}
-                </Highlight>
-              
-            </Box>
-          </div>
-        </Paper>
-      </ThemeProvider >
+          <Box style={{ backgroundColor: 'transparent', margin: '0% 0% 0% 0%', width: "100%", minHeight: '93%', maxHeight: '93%', overflowY: "scroll", padding: "0px 0px 0px 0px" }}>
+
+            <Highlight className="javascript">
+              {resolver ? props.schema : props.text}
+            </Highlight>
+
+          </Box>
+        </div>
+      </Paper>
+    </ThemeProvider >
   )
 }
