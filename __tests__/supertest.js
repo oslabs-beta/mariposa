af5076bc0e2ab280fa1bb98ad876e690e8895f3f
James Maguire <jmaguire655@gmail.com>
2022-01-11 13:39:19 -0500

    Merge from Dev (#52)
    * Docker (#49)
    
    * james tree edits
    
    * yeet
    
    * fixed more mergies
    
    * cleanup
    
    * deleted a lot of stuff
    
    * deleted more
    
    * deleted a bunch
    
    * james edits
    
    * got rid of dependencies
    
    * console logs gone and  docker in
    
    * james changes
    
    * latest commit
    
    * latest changes
    
    * changes made on Jan 11
    
    * Docker (#51)
    
    * james tree edits
    
    * yeet
    
    * fixed more mergies
    
    * cleanup
    
    * deleted a lot of stuff
    
    * deleted more
    
    * deleted a bunch
    
    * james edits
    
    * got rid of dependencies
    
    * console logs gone and  docker in
    
    * james changes
    
    * latest commit
    
    * latest changes
    
    * changes made on Jan 11
    
    * changes made Jan 11
    
    * changes made 1.11

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -6,16 +6,16 @@ const supertest = require("supertest");
 // sample test
 app.get("/test", async (req, res) => {
     res.json({ message: "pass!" });
-  });
+});
 
 
-  it("gets the test endpoint", async done => {
+it("gets the test endpoint", async done => {
     const response = await request.get("/test");
 
     expect(response.status).toBe(200);
     expect(response.body.message).toBe("pass!");
     done();
-  });
+});
 
 describe('Server Tests', () => {
     describe('/', () => {
@@ -24,42 +24,40 @@ describe('Server Tests', () => {
             it('responds with 200 status and text/html content type', () => {
                 return request(server)
                     .get('/')
-                        .expect('Content-type', /text\/html/)
-                        .expect(200);
+                    .expect('Content-type', /text\/html/)
+                    .expect(200);
             });
         });
     });
 
-describe("POST /users", () => {
+    describe("POST /users", () => {
 
-    describe("given a username and password", () => {
-        // should save a username and password to the DB
+        describe("given a username and password", () => {
+            // should save a username and password to the DB
 
-        // should respond with a json object containing the user id
+            // should respond with a json object containing the user id
 
-        test("should respond with a 200 status code", () => {
-            const response = request(app).post("/users").send({
-                username: "username",
-                password: "password"
+            test("should respond with a 200 status code", () => {
+                const response = request(app).post("/users").send({
+                    username: "username",
+                    password: "password"
+                })
+                expect(response.statusCode).toBe(200)
             })
-            expect(response.statusCode).toBe(200)
-        })
-        // should specify json in the content type header
-        test("should specify json in the content type header", async () => {
-            const response = await request(app).post("/users").send({
-                username: "username",
-                password: "password"
+            // should specify json in the content type header
+            test("should specify json in the content type header", async () => {
+                const response = await request(app).post("/users").send({
+                    username: "username",
+                    password: "password"
+                })
+                expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+
             })
-            expect(response.headers['content-type']).toEqual(expect.stringContaining("json"))
+        })
+
+        describe("when the username and password is missing", () => {
 
         })
-    })
-
-    describe("when the username and password is missing", () => {
 
     })
-
 })
-
-
-
